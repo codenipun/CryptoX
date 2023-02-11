6804da66772aa0c181ee52c0cd433d9561b0716d
Nipun Jain <nipunjain119@gmail.com>
2023-02-11 22:59:00 +0530

    Final commit

--- src/Components/CryptoNews.jsx
+++ src/Components/CryptoNews.jsx
@@ -14,27 +14,27 @@ const CryptoNews = ({simplified}) => {
   const [error, setError] = useState(false);
   const [page, setPage] = useState(1);
 
-  
-
   useEffect(() => {
     const options = {
-        method: 'GET',
-        url: 'https://bing-news-search1.p.rapidapi.com/news/search',
-        params: {q: 'Cryptocurrencies', count: simplified ? 8 : 39, freshness: 'Day', textFormat: 'Raw', safeSearch: 'Off'},
-        headers: {
-          'X-BingApis-SDK': 'true',
-          'X-RapidAPI-Key': '5e54334c51msh6ecd5f485093597p10b84bjsn3c45f4caea72',
-          'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
-        }
-      };
+      method: 'GET',
+      url: 'https://crypto-feed.p.rapidapi.com/news',
+      headers: {
+        'X-RapidAPI-Key': '5e54334c51msh6ecd5f485093597p10b84bjsn3c45f4caea72',
+        'X-RapidAPI-Host': 'crypto-feed.p.rapidapi.com'
+      }
+    };
     const fetchCoins = async() =>{
       try {
-        axios.request(options).then(function (response) {
-            // console.log(response.data.value);
-            setLoading(false);
-            setCoinsNews(response.data.value);
-        });
-        
+        // axios.request(options).then(function (response) {
+        //     console.log(response.data);
+        //     setLoading(false);
+        //     setCoinsNews(response);
+        // });
+
+        const response = await axios.get('https://newsdata.io/api/1/news?apikey=pub_14554f784f469f932d5abfd8ddd94f8f93b5f&q=crypto');
+        console.log(response.data.results);
+        setLoading(false);
+        setCoinsNews(response.data.results);
       } catch (error) {
         setError(true);
         setLoading(false);
@@ -60,13 +60,13 @@ const CryptoNews = ({simplified}) => {
                         <div key={j}>
                         <NewsCard
                             simplified={simplified} 
-                            title={i.name}
+                            title={i.title}
                             desc={i.description} 
-                            provider={i.provider[0]?.name}
-                            date={i.datePublished} 
-                            img={i.image?.thumbnail?.contentUrl}
-                            url={i.url}
-                            providerimg = {i.provider[0]?.image?.thumbnail?.contentUrl}
+                            provider={i.creator[0]}
+                            date={i.pubDate} 
+                            img={i.image_url}
+                            url={i.link}
+                            // providerimg = {i.favicon}
                         />
                         </div>
                     ))}
--- src/Components/NewsCard.jsx
+++ src/Components/NewsCard.jsx
@@ -7,6 +7,18 @@ import { isMotionComponent } from 'framer-motion'
 import moment from 'moment'
 
 const NewsCard = ({title, desc, provider, date, img, url, providerimg, simplified}) => {
+    const images = [
+        "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/bitcoinreuters_13072022.jpg?VersionId=IOd.5O9.N7AwoFQWBsc6XxrQkbrhy9U4&size=690:388",
+        "https://imgnew.outlookindia.com/uploadimage/library/16_9/16_9_5/IMAGE_1670500990.jpg",
+        "https://cloudfront-us-east-1.images.arcpublishing.com/coindesk/PJTR3KRDWJCRVE3QREM6KUOK7A.png",
+        "https://ichef.bbci.co.uk/news/976/cpsprodpb/DECC/production/_128263075_crypto_currency_getty.jpg.webp",
+        "https://www.investopedia.com/thmb/XsT78eXSXfqdt-lYLJtYha6gETk=/302x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/INV_CryptoCoinImage_17_WeekofJan302023-2f880fef219543f38e5135e8ff393872.jpg"
+    ]
+    // console.log(img);
+    if(img===null){
+        let randomNumber = getRandomNumber(0, 4);
+        img = images[randomNumber];
+    }
   return (
       <div className="newsCard">
     <a href={url} target={"blank"}>
@@ -29,6 +41,9 @@ const NewsCard = ({title, desc, provider, date, img, url, providerimg, simplifie
         </div>
         
   )
+}
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 export default NewsCard
