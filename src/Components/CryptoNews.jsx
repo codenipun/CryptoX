66d3205502b68483435238dedced4bb12dee8b05
Nipun Jain <nipunjain119@gmail.com>
2023-02-12 13:48:45 +0530

    final commit

--- src/Components/CryptoNews.jsx
+++ src/Components/CryptoNews.jsx
@@ -14,27 +14,32 @@ const CryptoNews = ({simplified}) => {
   const [error, setError] = useState(false);
   const [page, setPage] = useState(1);
 
+  let limit = 100;
+  if(simplified){
+    limit = 10;
+  }
   useEffect(() => {
     const options = {
       method: 'GET',
-      url: 'https://crypto-feed.p.rapidapi.com/news',
+      url: 'https://real-time-news-data.p.rapidapi.com/search',
+      params: {query: 'Cryptocurrencies,crypto', country: 'US', lang: 'en'},
       headers: {
         'X-RapidAPI-Key': '5e54334c51msh6ecd5f485093597p10b84bjsn3c45f4caea72',
-        'X-RapidAPI-Host': 'crypto-feed.p.rapidapi.com'
+        'X-RapidAPI-Host': 'real-time-news-data.p.rapidapi.com'
       }
     };
     const fetchCoins = async() =>{
       try {
-        // axios.request(options).then(function (response) {
-        //     console.log(response.data);
-        //     setLoading(false);
-        //     setCoinsNews(response);
-        // });
+        axios.request(options).then(function (response) {
+            console.log(response.data.data);
+            setLoading(false);
+            setCoinsNews(response.data.data);
+        });
 
-        const response = await axios.get('https://newsdata.io/api/1/news?apikey=pub_14554f784f469f932d5abfd8ddd94f8f93b5f&q=crypto');
-        console.log(response.data.results);
-        setLoading(false);
-        setCoinsNews(response.data.results);
+        // const response = await axios.get('https://newsdata.io/api/1/news?apikey=pub_14554f784f469f932d5abfd8ddd94f8f93b5f&q=crypto');
+        // console.log(response.data.results);
+        // setLoading(false);
+        // setCoinsNews(response.data.results);
       } catch (error) {
         setError(true);
         setLoading(false);
@@ -57,18 +62,19 @@ const CryptoNews = ({simplified}) => {
                 </div>
                 <div className="newsContainer">
                     {coinsNews.map((i,j)=>(
-                        <div key={j}>
+                        j<limit && (<div key={j}>
+                        
                         <NewsCard
                             simplified={simplified} 
                             title={i.title}
-                            desc={i.description} 
-                            provider={i.creator[0]}
-                            date={i.pubDate} 
-                            img={i.image_url}
+                            // desc={i.description} 
+                            // provider={i.creator[0]}
+                            date={i.published_datetime_utc} 
+                            img={i.photo_url}
                             url={i.link}
-                            // providerimg = {i.favicon}
+                            providerimg = {i.source_logo_url}
                         />
-                        </div>
+                        </div>)
                     ))}
                     {simplified && <div className='moreNews'>
                     <Link to={'/news'}>
